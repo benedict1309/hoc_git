ce9a29a0bbc1dfe6fd5b14375de4909a1b0c07a3
benedict1309 <luuthaison8a@gmail.com>
2026-05-10 17:10:46 +0700

    Update TRR1002.cpp

--- TRR1002.cpp
+++ TRR1002.cpp
@@ -1,1 +1,50 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+main()
+{
+    freopen("DT.INP","r",stdin);
+    freopen("DT.OUT","w",stdout);
+    int t,n; cin >> t >> n;
+    int a[n+1][n+1];
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    if(t==1)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            int dem = 0;
+            for(int j=1;j<=n;j++)
+            {
+                if(a[i][j]==1) dem++;
+            }
+            cout << dem << " ";
+        }
+    }
+    else{
+        queue<int> q;
+        cout << n << endl;
+        for(int i=1;i<=n;i++)
+        {
+            int dem = 0;
+            for(int j=1;j<=n;j++)
+            {
+                if(a[i][j]==1)
+                {
+                    dem++;
+                    q.push(j);
+                }
+            }
+           cout << dem << " ";
+           while(q.empty()==0){
+            cout << q.front() << " ";
+            q.pop();
+           }
+           cout << endl;
+        }
+    }
+}
